dc7d663c94496191f94c07c7796fff6633a6e55d
Pavel Belder <>
2023-05-13 16:29:59 +0300

    change numbers

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 //https://quiet-dawn-06541.herokuapp.com
 //https://dashboard.heroku.com/apps/quiet-dawn-06541/resources
 
+ //https://gold-jolly-dog.cyclic.app
+ //https://snopy.cyclic.app
+
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
   const data = [
-    { string: 'hello kaka', number: 41 },
-    { string: 'hello pipi', number: 42 },
-    { string: 'hello whatever', number: 43 }
+    { string: 'hello kaka', number: 51 },
+    { string: 'hello pipi', number: 52 },
+    { string: 'hello whatever', number: 53 }
   ];
 
   res.json(data);
